e6ca89c634c36358925b0e5ee0ae402868c39879
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2018-04-02 11:22:22 +0200

    Add initial permissions and roles support

---
+++ hsdp/resource_iam_permission.go
@@ -0,0 +1,75 @@
+package hsdp
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	iamclient "github.com/loafoe/go-hsdpiam"
+)
+
+func resourceIAMPermission() *schema.Resource {
+	return &schema.Resource{
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Create: resourceIAMPermissionCreate,
+		Read:   resourceIAMPermissionRead,
+		Update: resourceIAMPermissionUpdate,
+		Delete: resourceIAMPermissionDelete,
+
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"category": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceIAMPermissionCreate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceIAMPermissionRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*iamclient.Client)
+
+	id := d.Id()
+	permission, _, err := client.Permissions.GetPermission(&iamclient.GetPermissionOptions{Name: &id})
+	if err != nil {
+		return err
+	}
+	d.Set("category", permission.Category)
+	d.Set("description", permission.Description)
+	d.Set("name", permission.Name)
+	d.Set("type", permission.Type)
+	d.SetId(permission.Name)
+	d.Set("_id", permission.ID)
+	return nil
+}
+
+func resourceIAMPermissionUpdate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceIAMPermissionDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceIAMPermissionImport(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
