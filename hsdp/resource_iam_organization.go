c12c28e4427488dc4718938dcf2e9550f3e1af42
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-06-09 16:26:27 +0200

    Implement create and update for organisations

--- hsdp/resource_iam_organization.go
+++ hsdp/resource_iam_organization.go
@@ -1,6 +1,9 @@
 package hsdp
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hsdp/go-hsdp-api/iam"
 )
@@ -33,11 +36,41 @@ func resourceIAMOrg() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"is_root_org": &schema.Schema{
+				Type:          schema.TypeBool,
+				Optional:      true,
+				ConflictsWith: []string{"parent_org_id"},
+			},
+			"parent_org_id": &schema.Schema{
+				Type:          schema.TypeString,
+				Optional:      true,
+				ConflictsWith: []string{"is_root_org"},
+			},
 		},
 	}
 }
 
 func resourceIAMOrgCreate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*iam.Client)
+
+	name := d.Get("name").(string)
+	description := d.Get("description").(string)
+	isRootOrg := d.Get("is_root_org").(bool)
+	if isRootOrg {
+		return errors.New("cannot create root orgs")
+	}
+	parentOrgID, ok := d.Get("parent_org_id").(string)
+	if !ok {
+		return errors.New("non root orgs must specify a `parent_org_id`")
+	}
+	org, resp, err := client.Organizations.CreateOrganization(parentOrgID, name, description)
+	if err != nil {
+		return err
+	}
+	if org == nil {
+		return fmt.Errorf("failed to create organization: %d", resp.StatusCode)
+	}
+	d.SetId(org.OrganizationID)
 	return nil
 }
 
@@ -56,13 +89,21 @@ func resourceIAMOrgRead(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceIAMOrgUpdate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*iam.Client)
+	id := d.Id()
+	org, _, err := client.Organizations.GetOrganizationByID(id)
+	if err != nil {
+		return err
+	}
+
+	if d.HasChange("description") {
+		description := d.Get("description").(string)
+		org.Description = description
+		client.Organizations.UpdateOrganization(*org)
+	}
 	return nil
 }
 
 func resourceIAMOrgDelete(d *schema.ResourceData, m interface{}) error {
-	return nil
-}
-
-func resourceIAMOrgImport(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
