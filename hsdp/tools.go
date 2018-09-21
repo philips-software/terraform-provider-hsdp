8a816606f8c7dc1c907ec6a2d58909f72c402b24
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-09-21 15:24:01 +0200

    Efficient permission mutation

---
+++ hsdp/tools.go
@@ -0,0 +1,16 @@
+package hsdp
+
+// difference returns the elements in a that aren't in b
+func difference(a, b []string) []string {
+	mb := map[string]bool{}
+	for _, x := range b {
+		mb[x] = true
+	}
+	ab := []string{}
+	for _, x := range a {
+		if _, ok := mb[x]; !ok {
+			ab = append(ab, x)
+		}
+	}
+	return ab
+}
