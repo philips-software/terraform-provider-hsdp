7e66423cf097bab116a859fddbb7b8c56f1e3402
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-09-24 13:19:21 +0200

    Add TDR getting started example

---
+++ examples/tdr/contracts.tf
@@ -0,0 +1,16 @@
+resource "hsdp_iam_role" "tdr_contract" {
+    name = "TDRCONTRACT"
+    managing_organization = "${var.tdr_org_id}"
+    permissions = [
+        "CONTRACT.READ", 
+        "CONTRACT.CREATE"
+    ]
+}
+
+resource "hsdp_iam_group" "tdr_contractadmin" {
+    name = "TDR Contract Admin Group"
+    managing_organization = "${var.tdr_org_id}"
+    description           = "Group for TDR Users with Contract roles"
+    roles                 = ["${hsdp_iam_role.tdr_contract.id}"]
+    users                 = []
+}
