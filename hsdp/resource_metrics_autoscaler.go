71c69b748be7631aca12e289386d67837e1f52d9
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-12-15 07:08:14 +0100

    Handle intermittent errors

--- hsdp/resource_metrics_autoscaler.go
+++ hsdp/resource_metrics_autoscaler.go
@@ -3,9 +3,13 @@ package hsdp
 import (
 	"context"
 	"fmt"
+	"github.com/cenkalti/backoff/v4"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/philips-software/go-hsdp-api/console"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -34,14 +38,16 @@ func resourceMetricsAutoscaler() *schema.Resource {
 				ForceNew: true,
 			},
 			"max_instances": {
-				Type:     schema.TypeInt,
-				Optional: true,
-				Default:  10,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      10,
+				ValidateFunc: validation.IntBetween(0, 1000000000),
 			},
 			"min_instances": {
-				Type:     schema.TypeInt,
-				Optional: true,
-				Default:  1,
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      1,
+				ValidateFunc: validation.IntBetween(0, 1000000000),
 			},
 			"enabled": {
 				Type:     schema.TypeBool,
@@ -59,14 +65,16 @@ func resourceMetricsAutoscaler() *schema.Resource {
 							Required: true,
 						},
 						"max": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  10000,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      10000,
+							ValidateFunc: validation.IntBetween(1, 1000000),
 						},
 						"min": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  10,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      10,
+							ValidateFunc: validation.IntBetween(1, 1000000),
 						},
 					},
 				},
@@ -82,14 +90,16 @@ func resourceMetricsAutoscaler() *schema.Resource {
 							Required: true,
 						},
 						"max": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  6000000,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      6000000,
+							ValidateFunc: validation.IntBetween(1, 6000000),
 						},
 						"min": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  300,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      300,
+							ValidateFunc: validation.IntBetween(1, 6000000),
 						},
 					},
 				},
@@ -105,14 +115,16 @@ func resourceMetricsAutoscaler() *schema.Resource {
 							Required: true,
 						},
 						"max": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  100,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      100,
+							ValidateFunc: validation.IntBetween(0, 100),
 						},
 						"min": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  20,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      20,
+							ValidateFunc: validation.IntBetween(0, 100),
 						},
 					},
 				},
@@ -129,14 +141,16 @@ func resourceMetricsAutoscaler() *schema.Resource {
 							Default:  false,
 						},
 						"max": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  100,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      100,
+							ValidateFunc: validation.IntBetween(0, 100),
 						},
 						"min": {
-							Type:     schema.TypeFloat,
-							Optional: true,
-							Default:  5,
+							Type:         schema.TypeFloat,
+							Optional:     true,
+							Default:      5,
+							ValidateFunc: validation.IntBetween(0, 100),
 						},
 					},
 				},
@@ -158,7 +172,7 @@ func resourceMetricsAutoscalerDelete(ctx context.Context, d *schema.ResourceData
 	instanceID := d.Get("metrics_instance_id").(string)
 	app.Name = d.Get("app_name").(string)
 	app.Enabled = false
-	result, _, err := client.Metrics.UpdateApplicationAutoscaler(instanceID, app)
+	result, err := updateWithRetry(client, instanceID, app)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -185,7 +199,7 @@ func resourceMetricsAutoscalerRead(ctx context.Context, d *schema.ResourceData, 
 	instanceID := d.Get("metrics_instance_id").(string)
 	name := d.Get("app_name").(string)
 
-	app, _, err := client.Metrics.GetApplicationAutoscaler(instanceID, name)
+	app, err := getWitRetry(client, instanceID, name)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -244,7 +258,7 @@ func resourceMetricsAutoscalerCreate(ctx context.Context, d *schema.ResourceData
 			}
 		}
 	}
-	created, _, err := client.Metrics.UpdateApplicationAutoscaler(instanceID, app)
+	created, err := updateWithRetry(client, instanceID, app)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -253,4 +267,42 @@ func resourceMetricsAutoscalerCreate(ctx context.Context, d *schema.ResourceData
 	}
 	d.SetId(instanceID + created.Name)
 	return diags
+}
+
+func updateWithRetry(client *console.Client, instanceID string, app console.Application) (*console.Application, error) {
+	var created *console.Application
+	operation := func() error {
+		var err error
+		var resp *console.Response
+		created, resp, err = client.Metrics.UpdateApplicationAutoscaler(instanceID, app)
+		return checkForIntermittentErrors(resp, err)
+	}
+	err := backoff.Retry(operation, backoff.NewExponentialBackOff())
+	return created, err
+}
+
+func getWitRetry(client *console.Client, instanceID string, name string) (*console.Application, error) {
+	var app *console.Application
+	operation := func() error {
+		var err error
+		var resp *console.Response
+		app, resp, err = client.Metrics.GetApplicationAutoscaler(instanceID, name)
+		return checkForIntermittentErrors(resp, err)
+	}
+	err := backoff.Retry(operation, backoff.NewExponentialBackOff())
+	return app, err
+}
+
+func checkForIntermittentErrors(resp *console.Response, err error) error {
+	if resp == nil || resp.StatusCode > 500 {
+		return err
+	}
+	if resp.StatusCode == http.StatusInternalServerError {
+		return backoff.Permanent(fmt.Errorf("console: %s %w", resp.Error.Message, err))
+	}
+	if resp.StatusCode == http.StatusBadRequest &&
+		strings.Contains(resp.Error.Message, "invalid character") {
+		return ErrIntermittent
+	}
+	return backoff.Permanent(err)
 }
