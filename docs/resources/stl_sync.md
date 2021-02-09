9db250b055823c0ed3beec3ab640802c03c529e2
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-02-09 15:11:01 +0100

    Fine device sync control with this resource
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

---
+++ docs/resources/stl_sync.md
@@ -0,0 +1,11 @@
+# hsdp_stl_sync
+Synchronizes device configuration. This resource can be used to batch sync requests
+of a device e.g. you can add all resource configs to the trigger hash and disable sync
+per resource to batch syncs down to a single one as part of the `apply` stage.
+
+## Argument reference
+* `serial_number` - (Required) Serial number of the device to sync
+* `triggers` - (Required, Hashmap) Create dependencies on other resources
+
+## Argument reference
+* `synced_at` - The time the sync was performed by this resource
---
+++ hsdp/resource_stl_sync.go
@@ -0,0 +1,69 @@
+package hsdp
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/philips-software/go-hsdp-api/stl"
+	"time"
+)
+
+func resourceSTLSync() *schema.Resource {
+	return &schema.Resource{
+		Description:   `The ` + "`hsdp_stl_sync`" + ` resource syncs device config to the actual device.`,
+		CreateContext: resourceSTLSyncCreate,
+		ReadContext:   resourceSTLSyncRead,
+		DeleteContext: resourceSTLSyncDelete,
+
+		Schema: map[string]*schema.Schema{
+			"triggers": {
+				Description: "A map of arbitrary strings that, when changed, will force the 'hsdp_stl_sync' resource to be replaced, re-sync conifg with the device.",
+				Type:        schema.TypeMap,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"serial_number": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"synced_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceSTLSyncDelete(_ context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	d.SetId("")
+	return diag.Diagnostics{}
+}
+
+func resourceSTLSyncRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	return diag.Diagnostics{}
+}
+
+func resourceSTLSyncCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	config := m.(*Config)
+	var diags diag.Diagnostics
+	var client *stl.Client
+	var err error
+
+	if endpoint, ok := d.GetOk("endpoint"); ok {
+		client, err = config.STLClient(endpoint.(string))
+	} else {
+		client, err = config.STLClient()
+	}
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	serialNumber := d.Get("serial_number").(string)
+	err = client.Devices.SyncDeviceConfig(ctx, serialNumber)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("hsdp_stl_sync: %w", err))
+	}
+	d.Set("synced_at", time.Now().Format(time.RFC3339))
+	return diags
+}
