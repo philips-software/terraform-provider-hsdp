a2ab85f0b735cc7e2107876fce06e30f78fcbb9e
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-03-10 20:42:43 +0100

    Add debugging mode
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/philips-software/terraform-provider-hsdp/hsdp"
+	"log"
 )
 
 var commit = "deadbeef"
@@ -13,9 +16,20 @@ var buildSource = "unknown"
 var buildVersion = release + "-" + commit + "." + date + "." + buildSource
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return hsdp.Provider(buildVersion)
-		},
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: func() *schema.Provider {
+		return hsdp.Provider(buildVersion)
+	}}
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/philips-software/hsdp", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+	plugin.Serve(opts)
 }
