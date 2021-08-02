faa9c8c07de7d4e7f456346f14a0a073dd13b4bf
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-08-02 09:51:38 +0200

    Improve PKI client error messages and fix docs
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- hsdp/errors.go
+++ hsdp/errors.go
@@ -4,7 +4,7 @@ import "github.com/pkg/errors"
 
 var (
 	ErrInstanceIDMismatch       = errors.New("instanceID mismatch")
-	ErrCannotCreateRootOrg      = errors.New("cannot create root orgs")
+	ErrCannotCreateRootOrg      = errors.New("cannot create root organizations")
 	ErrMissingParentOrgID       = errors.New("missing parent_org_id")
 	ErrMissingClientID          = errors.New("missing Oauth2 client id")
 	ErrMissingClientPassword    = errors.New("missing OAuth2 client password")
@@ -18,4 +18,5 @@ var (
 	ErrDeleteSubscriptionFailed = errors.New("delete subscription failed")
 	ErrMissingOrganizationID    = errors.New("missing organization ID")
 	ErrMissingIAMCredentials    = errors.New("missing IAM credentials in the hsdp provider block. Add an IAM service identity or ORG admin with proper permissions")
+	ErrMissingUAACredentials    = errors.New("missing/invalid UAA credentials in the hsdp provider block")
 )
