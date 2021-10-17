646fc1a565e331d8e87cfceb57f9aa6096c95314
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-10-17 19:59:58 +0200

    IAM Users: add hsdp_iam_users data source with filtering
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- hsdp/provider.go
+++ hsdp/provider.go
@@ -114,7 +114,7 @@ func Provider(build string) *schema.Provider {
 				Optional:     true,
 				Description:  descriptions["uaa_username"],
 				RequiredWith: []string{"uaa_password"},
-				DefaultFunc:  schema.EnvDefaultFunc(UAAPassword, nil),
+				DefaultFunc:  schema.EnvDefaultFunc(UAAUsername, nil),
 			},
 			"uaa_password": {
 				Type:         schema.TypeString,
@@ -277,6 +277,7 @@ func Provider(build string) *schema.Provider {
 			"hsdp_ai_workspace":                      dataSourceAIWorkspace(),
 			"hsdp_iam_group":                         dataSourceIAMGroup(),
 			"hsdp_iam_role":                          dataSourceIAMRole(),
+			"hsdp_iam_users":                         dataSourceIAMUsers(),
 		},
 		ConfigureContextFunc: providerConfigure(build),
 	}
