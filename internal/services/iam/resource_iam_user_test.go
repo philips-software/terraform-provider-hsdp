e3c077cefe52e0a08c59657d8df6c841cf8e930e
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-05-16 00:37:28 +0200

    Test: add IAM user basic test
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

---
+++ internal/services/iam/resource_iam_user_test.go
@@ -0,0 +1,45 @@
+package iam_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/philips-software/terraform-provider-hsdp/internal/acctest"
+)
+
+func TestAccResourceIAMUser_basic(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "hsdp_iam_user.test"
+	parentOrgID := acctest.AccIAMOrgGUID()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(t)
+		},
+		ProviderFactories: acctest.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       testAccResourceIAMUser(parentOrgID),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "organization_id", parentOrgID),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceIAMUser(parentOrgID string) string {
+	return fmt.Sprintf(`
+resource "hsdp_iam_user" "test" {
+  login           = "developer"
+  email           = "acceptance@terrakube.com"
+  first_name      = "Devel"
+  last_name       = "Oper"
+  password        = "DoNot@123"
+  organization_id = %[1]q
+ 
+}`, parentOrgID)
+}
