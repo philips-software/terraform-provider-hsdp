8bf1116517a82e699efb961e0cbc5227e70af064
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-05-17 21:51:23 +0200

    Config: Add first acceptance test
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

---
+++ internal/services/configuration/data_source_config_test.go
@@ -0,0 +1,39 @@
+package configuration_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/philips-software/terraform-provider-hsdp/internal/acc"
+)
+
+func TestAccDataSourceConfig_basic(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "data.hsdp_config.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acc.PreCheck(t)
+		},
+		ProviderFactories: acc.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       testAccDataSourceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "url", "https://iam-client-test.us-east.philips-healthsuite.com"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceConfig() string {
+	return `
+data "hsdp_config" "test" {
+  service     = "iam"
+  region      = "us-east"
+  environment = "client-test"
+}`
+}
