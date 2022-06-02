e7fb34b3cb9015d32ba8d90ef9384f03a19dcb0f
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-06-02 08:40:46 +0200

    CDR: add basic CDR Subscription acceptance test
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

---
+++ internal/services/cdr/subscription/resource_cdr_subscription_test.go
@@ -0,0 +1,140 @@
+package subscription_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/philips-software/terraform-provider-hsdp/internal/acc"
+)
+
+func TestAccResourceCDRSubscription_basic(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "hsdp_cdr_subscription.test"
+	parentOrgID := acc.AccIAMOrgGUID()
+	cdrURL := acc.AccCDRURL()
+
+	randomNameSTU3 := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+	randomNameR4 := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+
+	// STU3
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acc.PreCheck(t)
+		},
+		ProviderFactories: acc.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       testAccResourceCDRSubscription(cdrURL, parentOrgID, randomNameSTU3, "stu3"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "status", "REQUESTED"),
+				),
+			},
+		},
+	})
+
+	// R4
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acc.PreCheck(t)
+		},
+		ProviderFactories: acc.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       testAccResourceCDRSubscription(cdrURL, parentOrgID, randomNameR4, "r4"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "status", "REQUESTED"),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceCDRSubscription(cdrURL, parentOrgID, name, version string) string {
+	return fmt.Sprintf(`
+
+data "hsdp_cdr_fhir_store" "sandbox" {
+  base_url = "%s"
+  fhir_org_id = hsdp_iam_org.test.id
+}
+
+data "hsdp_iam_introspect" "terraform" {
+}
+
+data "hsdp_iam_service" "service" {
+  service_id = data.hsdp_iam_introspect.terraform.subject
+}
+
+resource "hsdp_iam_org" "test" {
+  name  = "%s"
+  description = "Acceptance Test CDR %s"
+  parent_org_id = "%s"
+}
+
+resource "hsdp_iam_role" "cdr_admin" {
+  managing_organization = hsdp_iam_org.test.id
+  name                  = "TF_CDR_ADMIN"
+  permissions = [
+    "ALL.READ",
+    "ALL.WRITE"
+  ]
+}
+
+resource "hsdp_iam_group" "cdr_admins" {
+  managing_organization = hsdp_iam_org.test.id
+  name                  = "TF_CDR_ADMIN"
+  description           = "CDR Admins"
+  roles                 = [hsdp_iam_role.cdr_admin.id]
+  users                 = []
+  services              = [data.hsdp_iam_service.service.id]
+}
+
+resource "hsdp_cdr_org" "test" {
+  fhir_store  = data.hsdp_cdr_fhir_store.sandbox.endpoint
+
+  name        = "%s"
+  org_id      = hsdp_iam_org.test.id
+
+  version     = "%s"
+}
+
+resource "hsdp_cdr_subscription" "test" {
+  fhir_store  = hsdp_cdr_org.test.fhir_store
+
+  criteria        = "Patient"
+  reason          = "Acceptance test %s"
+  endpoint        = "https://webhook.myapp.io/patient"
+  delete_endpoint = "https://webhook.myapp.io/patient_deleted"
+  headers = [
+    "Authorization: Basic cm9uOnN3YW5zb24="
+  ]
+
+  version = "%s"
+
+  end = "2099-12-31T23:59:59Z"
+}
+
+`,
+		// DATA SOURCE
+		cdrURL,
+
+		// IAM ORG
+		name,
+		name,
+		parentOrgID,
+
+		// IAM GROUP
+
+		// CDR ORG
+		name,
+		version,
+
+		// CDR SUBSCRIPTION
+		name,
+		version,
+	)
+}
