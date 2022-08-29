3c34021227438459547902a1068ac2fbaf57bf19
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-08-29 12:29:30 +0200

    Bump go-hsdp-api version

--- internal/services/dicom/resource_dicom_repository.go
+++ internal/services/dicom/resource_dicom_repository.go
@@ -12,6 +12,7 @@ import (
 
 func ResourceDICOMRepository() *schema.Resource {
 	return &schema.Resource{
+		SchemaVersion: 1,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
