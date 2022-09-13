d84687439140879f44b551b547e9a6d87e3116f0
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-09-13 23:37:38 +0200

    IAM Device: initial implementation of IAM Device support #244
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

---
+++ internal/services/iam/device/resource_iam_device_test.go
@@ -0,0 +1,97 @@
+package device_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/philips-software/terraform-provider-hsdp/internal/acc"
+)
+
+func TestAccResourceIAMDevice_basic(t *testing.T) {
+	t.Parallel()
+
+	resourceName := "hsdp_iam_device.test"
+	parentOrgID := acc.AccIAMOrgGUID()
+	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlpha)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			acc.PreCheck(t)
+		},
+		ProviderFactories: acc.ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       testAccResourceIAMUser(parentOrgID, randomName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "login_id", randomName),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceIAMUser(parentOrgID, name string) string {
+	upperName := strings.ToUpper(name)
+
+	return fmt.Sprintf(`
+resource "hsdp_iam_org" "test" {
+  name = "ACC-%s"
+  description = "IAM Device Test %s"
+
+  parent_org_id = "%s"
+  wait_for_delete = false
+}
+
+resource "hsdp_iam_proposition" "test" {
+   name = "ACC-%s"
+   description = "IAM Device Test %s"
+   
+   organization_id = hsdp_iam_org.test.id
+}
+
+resource "hsdp_iam_application" "test" {
+    name = "ACC-%s"
+    description = "IAM Device Test %s"
+    proposition_id = hsdp_iam_proposition.test.id
+}
+
+resource "hsdp_iam_device" "test" {
+  login_id = "%s"
+  password = "Random%s@123!"
+
+  organization_id = hsdp_iam_org.test.id
+  application_id  = hsdp_iam_application.test.id
+
+  external_identifier {
+    type {
+      code = "ID"
+      text = "Device Identifier"
+    }
+    system = "http://www.philips.co.id/phs/healthwatch"
+    value = "%s"
+  }
+
+  type = "ActivityMonitor"
+
+  for_test = true
+  is_active = true
+}`,
+		// ORG
+		upperName,
+		name,
+		parentOrgID,
+		// PROP
+		upperName,
+		name,
+		// APP
+		upperName,
+		name,
+		// DEVICE
+		name,
+		name,
+		name)
+}
