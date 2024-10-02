111b1f43ada7eda02bd76ab73512495a4f865279
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2024-10-02 09:36:56 +0200

    Implement Proposition delete
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/loafoe/easyssh-proxy/v2 v2.0.4
 	github.com/loafoe/ferrite v0.2.0
 	github.com/philips-labs/siderite v0.16.6
-	github.com/philips-software/go-hsdp-api v0.85.0
+	github.com/philips-software/go-hsdp-api v0.86.0
 	github.com/pkg/errors v0.9.1
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/stretchr/testify v1.9.0
@@ -38,7 +38,7 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.22.0 // indirect
+	github.com/go-playground/validator/v10 v10.22.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
--- internal/services/iam/proposition/resource_iam_proposition.go
+++ internal/services/iam/proposition/resource_iam_proposition.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"net/http"
+	"time"
 
 	"github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -28,6 +30,7 @@ func ResourceIAMProposition() *schema.Resource {
 
 		CreateContext: resourceIAMPropositionCreate,
 		ReadContext:   resourceIAMPropositionRead,
+		UpdateContext: resourceIAMPropositionUpdate,
 		DeleteContext: resourceIAMPropositionDelete,
 
 		Schema: map[string]*schema.Schema{
@@ -53,8 +56,18 @@ func ResourceIAMProposition() *schema.Resource {
 				ForceNew:         true,
 				DiffSuppressFunc: tools.SuppressWhenGenerated,
 			},
+			"wait_for_delete": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Blocks until the proposition delete has completed. Default: false. The proposition delete process can take some time as all its associated resources like applications and services are removed recursively. This option is useful for ephemeral environments where the same proposition might be recreated shortly after a destroy operation.",
+			},
 		},
 	}
+}
+
+func resourceIAMPropositionUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	// NOOP
+	return resourceIAMPropositionRead(ctx, d, m)
 }
 
 func resourceIAMPropositionCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
@@ -146,12 +159,57 @@ func resourceIAMPropositionRead(_ context.Context, d *schema.ResourceData, m int
 	return diags
 }
 
-func resourceIAMPropositionDelete(_ context.Context, d *schema.ResourceData, _ interface{}) diag.Diagnostics {
-	// As HSDP IAM does not support IAM proposition deletion we simply
-	// clear the proposition from state. This will be properly implemented
-	// once the IAM API balances out
-	var diags diag.Diagnostics
-	d.SetId("")
+func resourceIAMPropositionDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*config.Config)
 
+	var diags diag.Diagnostics
+
+	client, err := c.IAMClient()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	id := d.Id()
+	prop, _, err := client.Propositions.GetPropositionByID(id)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	waitForDelete := d.Get("wait_for_delete").(bool)
+
+	ok, _, err := client.Propositions.DeleteProposition(*prop)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if !ok {
+		return diag.FromErr(config.ErrInvalidResponse)
+	}
+	if waitForDelete {
+		stateConf := &retry.StateChangeConf{
+			Pending:    []string{"IN_PROGRESS", "QUEUED", "indeterminate"},
+			Target:     []string{"SUCCESS"},
+			Refresh:    checkPropDeleteStatus(client, id),
+			Timeout:    d.Timeout(schema.TimeoutCreate),
+			Delay:      5 * time.Second,
+			MinTimeout: time.Duration(5) * time.Second,
+		}
+		_, err = stateConf.WaitForStateContext(ctx)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("waiting for delete: %w", err))
+		}
+	}
 	return diags
+}
+
+func checkPropDeleteStatus(client *iam.Client, id string) retry.StateRefreshFunc {
+	return func() (result interface{}, state string, err error) {
+		propStatus, resp, err := client.Propositions.DeleteStatus(id)
+		if err != nil {
+			return resp, "FAILED", err
+		}
+		if propStatus != nil {
+			return resp, propStatus.Status, nil
+		}
+		// We may need to return an error here
+		return resp, "IN_PROGRESS", nil
+	}
 }
